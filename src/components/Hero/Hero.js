36e0f1175a5cbe79023c3c99489f98236600c886
Taylor Zanke <taylor.zanke@gmail.com>
2020-12-22 08:59:25 -0800

    add change current image logic on hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import './Hero.scss';
 import Section from '../Section/Section'
 import Text from '../ui/Text/Text'
 import HeroImages from '../../constants/hero/hero'
 
-const Hero = ({}) => (
-  <Section className="hero">
-    
-    <div className="hero-background" style={{backgroundImage: `url(${HeroImages[1].imageUrl})`}}/>
+const Hero = ({}) => {
+  const [currentImage, setCurrentImage] = useState(0);
 
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={2}>
-        <Text>Figure Press</Text>
+  useEffect(() => {
+    const next = (currentImage + 1) % HeroImages.length;
+    const id = setTimeout(() => setCurrentImage(next), 5000);
+    return () => clearTimeout(id);
+  }, [currentImage]);
+
+
+  return (
+    <Section className="hero">
+      
+      <div className="hero-background" style={{backgroundImage: `url(${HeroImages[currentImage].imageUrl})`}}/>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={2}>
+          <Text>Figure Press</Text>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Text bold>
+            Debut, self-titled, book <em>Figures</em><br/>is due for release in 2021.
+          </Text>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Text paragraph>
+            Figure Press is a publishing project which uses the form of the book, and the processes of its production, to develop and sustain practices for thinking, making, and collaborating. Figure Press eagerly examines the process of form-making, of shaping and representing, in the hope of discovering practical tools for stabilizing the everyday demands of artistic practice. Through close collaboration, friendly critique, and often intentionally prolonged conversation, this press strives to provide encouraging and assistive contexts for artists developing delicate new work.
+          </Text>
+          <Text paragraph>
+            Figure Press is a collaboration between Matt Ransom and Taylor Zanke.
+          </Text>
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={3}>
-        <Text bold>
-          Debut, self-titled, book <em>Figures</em><br/>is due for release in 2021.
-        </Text>
-      </Grid>
-      <Grid item xs={12} sm={3}>
-        <Text paragraph>
-          Figure Press is a publishing project which uses the form of the book, and the processes of its production, to develop and sustain practices for thinking, making, and collaborating. Figure Press eagerly examines the process of form-making, of shaping and representing, in the hope of discovering practical tools for stabilizing the everyday demands of artistic practice. Through close collaboration, friendly critique, and often intentionally prolonged conversation, this press strives to provide encouraging and assistive contexts for artists developing delicate new work.
-        </Text>
-        <Text paragraph>
-          Figure Press is a collaboration between Matt Ransom and Taylor Zanke.
-        </Text>
-      </Grid>
-    </Grid>
-  </Section>
-)
+    </Section>
+  )
+}
 
 export default Hero
