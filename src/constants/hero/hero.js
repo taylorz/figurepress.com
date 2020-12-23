1ce622cb3f95c6e3f10eb72f974731d6b9f24047
Taylor Zanke <taylor.zanke@gmail.com>
2020-12-22 17:51:29 -0800

    replace images, update favicon

--- src/constants/hero/hero.js
+++ src/constants/hero/hero.js
@@ -1,27 +1,18 @@
+import image1 from '../../assets/1.png'
+import image2 from '../../assets/2.png'
+
 const HeroImages = [
   {
-    imageUrl: "http://irmaboom.nl/images/image1.jpg",
+    imageUrl: image1,
     isLight: true,
     publication: "Figures",
     caption: "Mockup, Detail 1" 
   },
   {
-    imageUrl: "http://irmaboom.nl/images/image2.jpg",
-    isLight: true,
+    imageUrl: image2,
+    isLight: false,
     publication: "Figures",
     caption: "Mockup, Detail 2" 
-  },
-  {
-    imageUrl: "http://irmaboom.nl/images/image21.jpg",
-    isLight: false,
-    publication: "Figures",
-    caption: "Mockup, Detail 3" 
-  },
-  {
-    imageUrl: "http://irmaboom.nl/images/image9.jpg",
-    isLight: false,
-    publication: "Figures",
-    caption: "Mockup, Detail 4" 
   },
 ]
 
